9c2d2cbde5f63038a07fe0be50700cd3a66cd152
邱建良 <70407821+s3543130@users.noreply.github.com>
2024-10-11 23:52:00 +0800

    1

--- scripts/AfterFreeSfx.cs
+++ scripts/AfterFreeSfx.cs
@@ -3,27 +3,27 @@ using Godot;
 namespace SystemOverride
 {
 
-    /// <summary>
-    /// A class that provides a convenient way to play a sound effect 
-    /// and automatically free the associated node from memory after playback is finished.
-    /// </summary>
-    public partial class AfterFreeSfx : AudioStreamPlayer2D
-    {
-        // Override method called when the node is ready to be used.
-        public override void _Ready()
-        {
-            // Attach a handler to the Finished signal.
-            Finished += OnFinished;
+	/// <summary>
+	/// A class that provides a convenient way to play a sound effect
+	/// and automatically free the associated node from memory after playback is finished.
+	/// </summary>
+	public partial class AfterFreeSfx : AudioStreamPlayer2D
+	{
+		// Override method called when the node is ready to be used.
+		public override void _Ready()
+		{
+			// Attach a handler to the Finished signal.
+			Finished += OnFinished;
 
-            // Start playing the sound effect.
-            Play();
-        }
+			// Start playing the sound effect.
+			Play();
+		}
 
-        // Handler for the Finished signal.
-        private void OnFinished()
-        {
-            // Free the node from memory.
-            QueueFree();
-        }
-    }
+		// Handler for the Finished signal.
+		private void OnFinished()
+		{
+			// Free the node from memory.
+			QueueFree();
+		}
+	}
 }
--- scripts/EnemySpawner.cs
+++ scripts/EnemySpawner.cs
@@ -4,51 +4,51 @@ using SystemOverride;
 
 namespace SystemOverride
 {
-    public partial class EnemySpawner : Node2D
-    {
-        [Export]
-        private PackedScene _enemySpaceship;
+	public partial class EnemySpawner : Node2D
+	{
+		[Export]
+		private PackedScene _enemySpaceship;
 
-        [Export]
-        private Spaceship _player;
+		[Export]
+		private Spaceship _player;
 
-        [Export]
-        private float _spawnRadius = 100f; // default spawn radius is 100 units
+		[Export]
+		private float _spawnRadius = 100f; // default spawn radius is 100 units
 
-        [Export]
-        private AIController _controller;
+		[Export]
+		private AIController _controller;
 
-        private RandomNumberGenerator _rng = new RandomNumberGenerator();
+		private RandomNumberGenerator _rng = new RandomNumberGenerator();
 
-        public override void _Ready()
-        {
-            _rng.Randomize();
-        }
+		public override void _Ready()
+		{
+			_rng.Randomize();
+		}
 
-        public override void _Process(double delta)
-        {
-            if (_controller.SpaceshipCount < 15)
-            {
-                SpawnEnemy();
-            }
-        }
+		public override void _Process(double delta)
+		{
+			if (_controller.SpaceshipCount < 15)
+			{
+				SpawnEnemy();
+			}
+		}
 
-        public void SpawnEnemy()
-        {
-            if (_player != null && _enemySpaceship != null)
-            {
-                // calculate a random angle on the circle
-                float angle = (float)_rng.RandfRange(0, Mathf.Tau);
+		public void SpawnEnemy()
+		{
+			if (_player != null && _enemySpaceship != null)
+			{
+				// calculate a random angle on the circle
+				float angle = (float)_rng.RandfRange(0, Mathf.Tau);
 
-                // calculate the position of the enemy spaceship
-                Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _spawnRadius;
-                Vector2 enemyPos = _player.Position + offset;
+				// calculate the position of the enemy spaceship
+				Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _spawnRadius;
+				Vector2 enemyPos = _player.Position + offset;
 
-                // spawn the enemy spaceship at the calculated position
-                Spaceship enemy = _enemySpaceship.Instantiate<Spaceship>();
-                enemy.Position = enemyPos;
-                _controller.AddSpaceship(enemy);
-            }
-        }
-    }
+				// spawn the enemy spaceship at the calculated position
+				Spaceship enemy = _enemySpaceship.Instantiate<Spaceship>();
+				enemy.Position = enemyPos;
+				_controller.AddSpaceship(enemy);
+			}
+		}
+	}
 }
