529e7199d03e733dad417b45389e5f12c47b1220
邱建良 <70407821+s3543130@users.noreply.github.com>
2024-10-12 03:30:46 +0800

    Reapply "1"
    This reverts commit 5ed67492ad4cb4aae22d5ad0e479e3a4c7d510c8.

--- scripts/AfterFreeGFX.cs
+++ scripts/AfterFreeGFX.cs
@@ -3,32 +3,32 @@ using Godot;
 namespace SystemOverride
 {
 
-    /// <summary>
-    /// Checks if the particle system has finished emitting and frees the node if true.
-    /// </summary>
-    public partial class AfterFreeGFX : GpuParticles2D
-    {
-        private const bool _emitOnStart = true;
+	/// <summary>
+	/// Checks if the particle system has finished emitting and frees the node if true.
+	/// </summary>
+	public partial class AfterFreeGFX : GpuParticles2D
+	{
+		private const bool _emitOnStart = true;
 
-        public override void _Ready()
-        {
-            Emitting = _emitOnStart;
-        }
+		public override void _Ready()
+		{
+			Emitting = _emitOnStart;
+		}
 
-        public override void _Process(double delta)
-        {
-            DestroyIfFinished();
-        }
+		public override void _Process(double delta)
+		{
+			DestroyIfFinished();
+		}
 
-        private void DestroyIfFinished()
-        {
+		private void DestroyIfFinished()
+		{
 
-            // No idea if emitting turn false when the particles stop
-            // emitting or when there are no particles.
-            if (!Emitting)
-            {
-                QueueFree();
-            }
-        }
-    }
+			// No idea if emitting turn false when the particles stop
+			// emitting or when there are no particles.
+			if (!Emitting)
+			{
+				QueueFree();
+			}
+		}
+	}
 }
