230af3a4e0c157d32fa4ddfad54f385ae2b93300
邱建良 <70407821+s3543130@users.noreply.github.com>
2024-10-12 03:30:47 +0800

    Revert "1"
    This reverts commit 9c2d2cbde5f63038a07fe0be50700cd3a66cd152.

--- scripts/Engine.cs
+++ scripts/Engine.cs
@@ -3,104 +3,104 @@ using System;
 
 namespace SystemOverride
 {
-	/// <summary>
-	/// The engines in these groups get activated at once. If the engine points backwards, it's thrust direction is forward
-	/// because it makes the spaceship go forward.
-	/// </summary>
-	public enum EngineMoveDirectionGroup
-	{
-		None, Forward, Backward, Left, Right
-	}
+    /// <summary>
+    /// The engines in these groups get activated at once. If the engine points backwards, it's thrust direction is forward
+    /// because it makes the spaceship go forward.
+    /// </summary>
+    public enum EngineMoveDirectionGroup
+    {
+        None, Forward, Backward, Left, Right
+    }
 
-	/// <summary>
-	/// The engines in these groups get activated together. If the engine is offseted to the right side of the spaceship,
-	/// it's turn direction group will be Left, because the engine will turn the spaceship to the left.
-	/// </summary>
-	public enum EngineTurnDirectionGroup
-	{
-		None, Left, Right
-	}
-
-
-	public partial class Engine : Node2D
-	{
-		// Exports
-
-		[Export]
-		public string EngineName { get; private set; }
-
-		[Export]
-		public EngineMoveDirectionGroup MoveDirectionGroup { get; private set; }
-
-		[Export]
-		public EngineTurnDirectionGroup TurnDirectionGroup { get; private set; }
-
-		[Export]
-		public float EngineForce { get; private set; }
-
-		[Export]
-		AudioStreamPlayer2D _optionalEngineAudioStreamPlayer = null;
-
-		[Export]
-		GpuParticles2D _optionalEngineParticles2D = null;
-
-		[Export]
-		Light2D _optionalEngineLight = null;
-
-		// Properties
-
-		public float EngineForceMultiplier { get; set; } = 1.0f;
-
-		public bool EngineEnabled { get; set; }
-
-		// Constants
-
-		// State
+    /// <summary>
+    /// The engines in these groups get activated together. If the engine is offseted to the right side of the spaceship,
+    /// it's turn direction group will be Left, because the engine will turn the spaceship to the left.
+    /// </summary>
+    public enum EngineTurnDirectionGroup
+    {
+        None, Left, Right
+    }
 
 
-		// Methods
+    public partial class Engine : Node2D
+    {
+        // Exports
 
-		/// <summary>
-		/// Adds thrust force to the spaceship. Needs to be called inside physics process.
-		/// </summary>
-		/// <param name="spaceship">Owner of the engine.</param>
-		/// <param name="forceFactor">Value between 0.0 to 1.0 specifiing how much thrust to allow from the engine.</param>
-		public void ApplyForceToOwner(Spaceship spaceship, float forceFactor)
-		{
-			Debug.Assert(forceFactor >= 0.0f && forceFactor <= 1.0f, "forceFactor >= 0.0f && forceFactor <= 1.0f");
-			Debug.Assert(IsInstanceValid(spaceship), "IsInstanceValid(spaceship)");
+        [Export]
+        public string EngineName { get; private set; }
 
-			if (EngineEnabled)
-			{
-				spaceship.ApplyForce(CalculateThrustDirection() * EngineForce * EngineForceMultiplier,
-					GlobalPosition - spaceship.GlobalPosition);
-			}
+        [Export]
+        public EngineMoveDirectionGroup MoveDirectionGroup { get; private set; }
 
-			// Activate effects if the engine is enabled.
+        [Export]
+        public EngineTurnDirectionGroup TurnDirectionGroup { get; private set; }
 
-			if (IsInstanceValid(_optionalEngineLight) && forceFactor > 0.0f)
-			{
-				_optionalEngineLight.Enabled = EngineEnabled;
-			}
+        [Export]
+        public float EngineForce { get; private set; }
 
-			if (IsInstanceValid(_optionalEngineParticles2D) && forceFactor > 0.0f)
-			{
-				_optionalEngineParticles2D.Emitting = EngineEnabled;
-			}
+        [Export]
+        AudioStreamPlayer2D _optionalEngineAudioStreamPlayer = null;
 
-			if (IsInstanceValid(_optionalEngineAudioStreamPlayer) && forceFactor > 0.0f)
-			{
-				_optionalEngineAudioStreamPlayer.Playing = EngineEnabled;
-			}
+        [Export]
+        GpuParticles2D _optionalEngineParticles2D = null;
 
-		}
+        [Export]
+        Light2D _optionalEngineLight = null;
 
-		/// <summary>
-		/// Calculates direction in which the thruster adds force to the spaceship.
-		/// </summary>
-		Vector2 CalculateThrustDirection()
-		{
-			return (ToGlobal(Vector2.Up / Scale) - GlobalPosition).Normalized();
-		}
-	}
+        // Properties
+
+        public float EngineForceMultiplier { get; set; } = 1.0f;
+
+        public bool EngineEnabled { get; set; }
+
+        // Constants
+
+        // State
+
+
+        // Methods
+
+        /// <summary>
+        /// Adds thrust force to the spaceship. Needs to be called inside physics process.
+        /// </summary>
+        /// <param name="spaceship">Owner of the engine.</param>
+        /// <param name="forceFactor">Value between 0.0 to 1.0 specifiing how much thrust to allow from the engine.</param>
+        public void ApplyForceToOwner(Spaceship spaceship, float forceFactor)
+        {
+            Debug.Assert(forceFactor >= 0.0f && forceFactor <= 1.0f, "forceFactor >= 0.0f && forceFactor <= 1.0f");
+            Debug.Assert(IsInstanceValid(spaceship), "IsInstanceValid(spaceship)");
+
+            if (EngineEnabled)
+            {
+                spaceship.ApplyForce(CalculateThrustDirection() * EngineForce * EngineForceMultiplier,
+                    GlobalPosition - spaceship.GlobalPosition);
+            }
+
+            // Activate effects if the engine is enabled.
+
+            if (IsInstanceValid(_optionalEngineLight) && forceFactor > 0.0f)
+            {
+                _optionalEngineLight.Enabled = EngineEnabled;
+            }
+
+            if (IsInstanceValid(_optionalEngineParticles2D) && forceFactor > 0.0f)
+            {
+                _optionalEngineParticles2D.Emitting = EngineEnabled;
+            }
+
+            if (IsInstanceValid(_optionalEngineAudioStreamPlayer) && forceFactor > 0.0f)
+            {
+                _optionalEngineAudioStreamPlayer.Playing = EngineEnabled;
+            }
+
+        }
+
+        /// <summary>
+        /// Calculates direction in which the thruster adds force to the spaceship.
+        /// </summary>
+        Vector2 CalculateThrustDirection()
+        {
+            return (ToGlobal(Vector2.Up / Scale) - GlobalPosition).Normalized();
+        }
+    }
 }
--- scripts/Projectile.cs
+++ scripts/Projectile.cs
@@ -3,83 +3,83 @@ using System;
 
 namespace SystemOverride
 {
-	public partial class Projectile : Node2D
-	{
-		[ExportGroup("General Settings")]
+    public partial class Projectile : Node2D
+    {
+        [ExportGroup("General Settings")]
 
-		[Export]
-		int _damage = 10;
+        [Export]
+        int _damage = 10;
 
-		[Export]
-		float _aliveTime = 10.0f;
+        [Export]
+        float _aliveTime = 10.0f;
 
-		[Export]
-		float _velocity = 1000.0f;
+        [Export]
+        float _velocity = 1000.0f;
 
-		[ExportGroup("Assets")]
+        [ExportGroup("Assets")]
 
-		[Export]
-		PackedScene _impactParticlesScene;
+        [Export]
+        PackedScene _impactParticlesScene;
 
-		[ExportGroup("Child Nodes")]
+        [ExportGroup("Child Nodes")]
 
-		[Export]
-		Area2D _collisionArea;
+        [Export]
+        Area2D _collisionArea;
 
-		[Export]
-		Timer _aliveTimer;
+        [Export]
+        Timer _aliveTimer;
 
-		public Vector2 InitialVelocity { get; set; }
+        public Vector2 InitialVelocity { get; set; }
 
-		public override void _Ready()
-		{
-			Debug.Assert(_damage > 0, "_damage > 0");
-			Debug.Assert(_aliveTime > 0, "_aliveTime > 0");
-			Debug.Assert(_velocity > 0, "_velocity > 0");
-			Debug.Assert(_impactParticlesScene != null, "_impactParticlesScene != null");
-			Debug.Assert(_collisionArea != null, "_collisionArea != null");
-			Debug.Assert(_aliveTimer != null, "_aliveTimer != null");
+        public override void _Ready()
+        {
+            Debug.Assert(_damage > 0, "_damage > 0");
+            Debug.Assert(_aliveTime > 0, "_aliveTime > 0");
+            Debug.Assert(_velocity > 0, "_velocity > 0");
+            Debug.Assert(_impactParticlesScene != null, "_impactParticlesScene != null");
+            Debug.Assert(_collisionArea != null, "_collisionArea != null");
+            Debug.Assert(_aliveTimer != null, "_aliveTimer != null");
 
-			_collisionArea.BodyEntered += _collisionArea_BodyEntered;
+            _collisionArea.BodyEntered += _collisionArea_BodyEntered;
 
-			_aliveTimer.Timeout += _aliveTimer_Timeout;
+            _aliveTimer.Timeout += _aliveTimer_Timeout;
 
-			_aliveTimer.Start(_aliveTime);
-		}
+            _aliveTimer.Start(_aliveTime);
+        }
 
-		private void _aliveTimer_Timeout()
-		{
-			QueueFree();
-		}
+        private void _aliveTimer_Timeout()
+        {
+            QueueFree();
+        }
 
-		private void _collisionArea_BodyEntered(Node2D body)
-		{
-			if (body is Spaceship spaceship)
-			{
-				GpuParticles2D fx = _impactParticlesScene.Instantiate<GpuParticles2D>();
-				GetTree().Root.AddChild(fx);
+        private void _collisionArea_BodyEntered(Node2D body)
+        {
+            if (body is Spaceship spaceship)
+            {
+                GpuParticles2D fx = _impactParticlesScene.Instantiate<GpuParticles2D>();
+                GetTree().Root.AddChild(fx);
 
-				fx.GlobalPosition = GlobalPosition;
-				fx.Emitting = true;
+                fx.GlobalPosition = GlobalPosition;
+                fx.Emitting = true;
 
-				spaceship.TakeDamage(_damage);
+                spaceship.TakeDamage(_damage);
 
-				QueueFree();
-			}
-		}
+                QueueFree();
+            }
+        }
 
-		public override void _Process(double delta)
-		{
-			const float weaponVelocityModificationFactor = 0.1f;
-			// Calculate the direction in which to move the node based on its current orientation and the desired direction of movement.
-			Translate((Transform.BasisXform(Vector2.Up).Normalized() * _velocity
-				+ InitialVelocity * weaponVelocityModificationFactor) * (float)delta);
-		}
+        public override void _Process(double delta)
+        {
+            const float weaponVelocityModificationFactor = 0.1f;
+            // Calculate the direction in which to move the node based on its current orientation and the desired direction of movement.
+            Translate((Transform.BasisXform(Vector2.Up).Normalized() * _velocity 
+                + InitialVelocity * weaponVelocityModificationFactor) * (float)delta);
+        }
 
-		public void SwitchToCollisionLayer(int layer)
-		{
-			_collisionArea.CollisionLayer = 0;
-			_collisionArea.CollisionMask = ~(1u << (layer - 1));
-		}
-	}
+        public void SwitchToCollisionLayer(int layer)
+        {
+            _collisionArea.CollisionLayer = 0;
+            _collisionArea.CollisionMask = ~(1u << (layer - 1));
+        }
+    }
 }
